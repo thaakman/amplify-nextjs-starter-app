911603594e57bd676915e90392e1f0bc1397270e
thaakman <33257914+thaakman@users.noreply.github.com>
2024-01-01 16:50:25 +0100

    Update page.tsx
    My first edit :)

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
+          My first edit :)
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
